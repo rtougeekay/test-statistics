8885105584cbab6c7a54682ca631fc70d9cbadb8
J Richard K Tougeekay <rtougeekay@gmail.com>
2026-04-16 10:36:40 -0500

    Updated version or exercise test statistics.

--- src/main/java/com/pluralsight/app.java
+++ src/main/java/com/pluralsight/app.java
@@ -1,4 +1,32 @@
 package com.pluralsight;
 
+import java.util.Arrays;
+
 public class app {
+    public static void main(String[] args) {
+
+        int[] scores = {85, 90, 75, 70, 92, 88, 95, 84, 89, 93};
+
+        int sum = 0;
+        Arrays.sort(scores);
+
+        for (int i = 0; i < scores.length; i++) {
+            sum += scores[i];
+        }
+        double average = (double) sum / scores.length;
+
+        int high = scores[0];
+        for (int i = 1; i < scores.length; i++) {
+            if (scores[i] > high) {
+                high = scores[i];
+            }
+        }
+        System.out.println("Average: " + average);
+    }
+
+
 }
+
+
+
+
